dc0e1fe66c17271b121a4084e108bb97d82f86b6
otter <otter@root.com>
2024-09-04 16:28:34 +0200

    debugging macros

---
+++ macros.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define SUCCESS(format, ...) DbgPrint("[] blister: " format, __VA_ARGS__)
+#define ERROR(format, ...) DbgPrint("[!] blister: " format, __VA_ARGS__)
+#define WARN(format, ...) DbgPrint("[WARNING] blister: " format, __VA_ARGS__)
+#define INFO(format, ...) DbgPrint("[INFO] blister: " format, __VA_ARGS__)
