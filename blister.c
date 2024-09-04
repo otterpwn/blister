6d35ada31a2002438104fc035abff324b2c98f85
otter <otter@root.com>
2024-09-04 20:34:33 +0200

    moved function declarations into core.h and created source file for function definition

--- blister.c
+++ blister.c
@@ -2,13 +2,10 @@
 
 #include "customTypes.h"
 #include "macros.h"
+#include "core.h"
 
 // function and globals definitions
 DRIVER_UNLOAD UnloadDriver;
-VOID ImageLoadNotifyCallback(IN OPTIONAL PUNICODE_STRING FullImageName, IN HANDLE ProcessId, IN PIMAGE_INFO ImageInfo);
-VOID ReportCallbacks(IN PVOID StartContext);
-VOID PCreateProcessNotifyRoutineEx(IN OUT PEPROCESS Process, IN HANDLE ProcessId, IN OUT OPTIONAL PPS_CREATE_NOTIFY_INFO CreateInfo);
-OB_PREOP_CALLBACK_STATUS PobPreOperationCallback(IN PVOID RegistrationContext, IN POB_PRE_OPERATION_INFORMATION OperationInformation);
 
 BlisterState driverState = { 0 };
 
