5a05c47305c38d766d04503b1fc02717eed176fb
otter <otter@root.com>
2024-09-05 17:24:47 +0200

    added function to handle callbacks for when a process is exiting: this handles removing the PP entry from the cache and memory cleanup

--- core.h
+++ core.h
@@ -6,3 +6,4 @@ VOID ReportCallbacks(IN PVOID StartContext);
 VOID ImageLoadNotifyCallback(IN OPTIONAL PUNICODE_STRING FullImageName, IN HANDLE ProcessId, IN PIMAGE_INFO ImageInfo);
 VOID PCreateProcessNotifyRoutineEx(IN OUT PEPROCESS Process, IN HANDLE ProcessId, IN OUT OPTIONAL PPS_CREATE_NOTIFY_INFO CreateInfo);
 OB_PREOP_CALLBACK_STATUS PobPreOperationCallback(IN PVOID RegistrationContext, IN POB_PRE_OPERATION_INFORMATION OperationInformation);
+VOID PCreateProcessNotifyExitingHandler(IN OUT PEPROCESS Process, IN HANDLE ProcessId, IN OUT OPTIONAL PPS_CREATE_NOTIFY_INFO CreateInfo);
