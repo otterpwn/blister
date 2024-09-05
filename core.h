f3137569a7ebc67b388b731a6a0911c78b585ac0
otter <otter@root.com>
2024-09-05 12:01:08 +0200

    function definitions for core.c

--- core.h
+++ core.h
@@ -2,7 +2,7 @@
 
 #include <ntddk.h>
 
-VOID ImageLoadNotifyCallback(IN OPTIONAL PUNICODE_STRING FullImageName, IN HANDLE ProcessId, IN PIMAGE_INFO ImageInfo);
 VOID ReportCallbacks(IN PVOID StartContext);
+VOID ImageLoadNotifyCallback(IN OPTIONAL PUNICODE_STRING FullImageName, IN HANDLE ProcessId, IN PIMAGE_INFO ImageInfo);
 VOID PCreateProcessNotifyRoutineEx(IN OUT PEPROCESS Process, IN HANDLE ProcessId, IN OUT OPTIONAL PPS_CREATE_NOTIFY_INFO CreateInfo);
 OB_PREOP_CALLBACK_STATUS PobPreOperationCallback(IN PVOID RegistrationContext, IN POB_PRE_OPERATION_INFORMATION OperationInformation);
