583bd4ccc1cafe38fff290bf5e8b7b8936089d9b
otter <otter@root.com>
2024-09-07 14:28:29 +0200

    added macro to count the number of elements of an array

--- macros.h
+++ macros.h
@@ -4,3 +4,5 @@
 #define ERROR(format, ...) DbgPrint("[!] blister: " format, __VA_ARGS__)
 #define WARN(format, ...) DbgPrint("[WARNING] blister: " format, __VA_ARGS__)
 #define INFO(format, ...) DbgPrint("[INFO] blister: " format, __VA_ARGS__)
+
+#define NUMBEROFELEMENTS(element) (sizeof(element) / sizeof((element)[0]))
